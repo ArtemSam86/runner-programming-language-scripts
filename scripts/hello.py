ab9224f0826c29103471126110f8c6b72580e998
Artem Samarskiy <a.samarskij@notamedia.ru>
2026-02-24 17:36:44 +0300

    Fixed hello.py script

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,1 +1,14 @@
-import sys, json; data = json.load(sys.stdin); print(json.dumps({"greeting": f"Hello, {data["name"]}"}))
+import json
+import os
+import sys
+
+data = json.load(sys.stdin)
+json_data = json.dumps(
+    {
+        "curdir": os.curdir,
+        "name": os.name,
+        "cpu_count": os.cpu_count(),
+    }
+)
+print(json_data)
+print(json.dumps(data))
